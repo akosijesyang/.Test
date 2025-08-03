17d869e82b8f1fd7abe7b465911fa112c5b95aa7
akosijesyang <84263753+akosijesyang@users.noreply.github.com>
2025-08-03 15:14:23 +0800

    Add files via upload

---
+++ HelloWorld_Privilege.ps1
@@ -0,0 +1,8 @@
+﻿#Created by MS Copilot
+
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+if ($identity.Name -eq "NT AUTHORITY\SYSTEM") {
+    Write-Host "Hello World, I am running as a SYSTEM"
+} else {
+    Write-Host "Hello World, but I'm NOT running as SYSTEM. I'm $($identity.Name)"
+}
